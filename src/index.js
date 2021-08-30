41b2a6838fef0458cd36624e0ab6f590f2b48afe
gortatka <gtatsiana@tut.by>
2021-08-30 22:41:17 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const arr = [];
+    for (const char of str) {
+        if (arr[arr.length - 1] == char) {
+            arr.pop();
+        } else {
+            const pair = bracketsConfig.find(pair => pair[0] === char);
+            if (pair) arr.push(pair[1]);
+            else return false;
+        }
+    }
+    return !arr.length;
 }
